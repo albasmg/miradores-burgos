da433786cc4a97f77c10ca850a7aac06e7bcfc57
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-11-15 19:40:44 +0100

    💄 Add mirador section styles

--- src/ui/pages/Mirador/Mirador.jsx
+++ src/ui/pages/Mirador/Mirador.jsx
@@ -1,15 +1,25 @@
-const Mirador = ({ subheaderTitle, sections }) => {
+const Mirador = ({ subheaderTitle, introduction, sections }) => {
   return (
-    <>
-      <div>{subheaderTitle}</div>
+    <section className="mirador">
+      <div className="mirador__subheader">
+        <h3 className="mirador__subheaderTitle">{subheaderTitle}</h3>
+      </div>
+      <div className="mirador__introduction">
+        <p className="mirador__introductionParagraph">
+          {introduction.text.firstParagraph}
+        </p>
+        <p className="mirador__introductionParagraph">
+          {introduction.text.secondParagraph}
+        </p>
+      </div>
       {sections.map(({ title, bodyText, linkText, images }) => (
-        <div key={title}>
-          <h3>{title}</h3>
-          <p>{bodyText}</p>
-          {linkText && <p>{linkText}</p>}
+        <div key={title} className="mirador__section">
+          <h4 className="mirador__sectionTitle">{title}</h4>
+          <p className="mirador__sectionText">{bodyText}</p>
+          {linkText && <p className="mirador__sectionLink">{linkText}</p>}
         </div>
       ))}
-    </>
+    </section>
   );
 };
 
