3ecfe04b7d475db5a4fea934c31ce2621a7b0ecc
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-12-03 22:12:28 +0100

    Change home and miradores photos

--- src/ui/pages/Miradores/Miradores.jsx
+++ src/ui/pages/Miradores/Miradores.jsx
@@ -1,78 +1,55 @@
 import { Link } from 'react-router-dom';
-import mirador1photo from '../../../assets/images/headers/mirador-1-cabecera.jpg';
-import mirador2photo from '../../../assets/images/headers/mirador-2-cabecera.jpg';
-import mirador3photo from '../../../assets/images/headers/mirador-3-cabecera.jpg';
-import mirador4photo from '../../../assets/images/headers/mirador-4-cabecera.jpg';
-import mirador5photo from '../../../assets/images/headers/mirador-5-cabecera.jpg';
-import mirador6photo from '../../../assets/images/headers/mirador-6-cabecera.jpg';
-import mirador7photo from '../../../assets/images/headers/mirador-7-cabecera.jpg';
-import mirador8photo from '../../../assets/images/headers/mirador-8-cabecera.jpg';
+import mirador1photo from '../../../assets/images/miradores/mirador1.jpg';
+import mirador2photo from '../../../assets/images/miradores/mirador2.jpg';
+import mirador3photo from '../../../assets/images/miradores/mirador3.jpg';
+import mirador4photo from '../../../assets/images/miradores/mirador4.jpg';
+import mirador5photo from '../../../assets/images/miradores/mirador5.jpg';
+import mirador6photo from '../../../assets/images/miradores/mirador6.jpg';
+import mirador7photo from '../../../assets/images/miradores/mirador7.jpg';
+import mirador8photo from '../../../assets/images/miradores/mirador8.jpg';
 
 const Miradores = () => {
   return (
     <div className="home">
       <Link to="/miradores/1">
-        <div
-          className="home__mirador"
-          style={{ backgroundImage: `url(${mirador1photo})` }}
-        >
-          <p className="home__title">Desde Museo Evolución</p>
+        <div className="home__mirador">
+          <img src={mirador1photo} alt="mirador1"></img>
         </div>
       </Link>
       <Link to="/miradores/2">
-        <div
-          className="home__mirador"
-          style={{ backgroundImage: `url(${mirador2photo})` }}
-        >
-          <p className="home__title">Desde El Espolón</p>{' '}
+        <div className="home__mirador">
+          <img src={mirador2photo} alt="mirador2"></img>
         </div>
       </Link>
       <Link to="/miradores/3">
-        <div
-          className="home__mirador"
-          style={{ backgroundImage: `url(${mirador3photo})` }}
-        >
-          <p className="home__title">Desde la Plaza Mayor</p>{' '}
+        <div className="home__mirador">
+          <img src={mirador3photo} alt="mirador3"></img>
         </div>
       </Link>
       <Link to="/miradores/4">
-        <div
-          className="home__mirador"
-          style={{ backgroundImage: `url(${mirador4photo})` }}
-        >
-          <p className="home__title">Desde Las Llanas</p>{' '}
+        <div className="home__mirador">
+          <img src={mirador4photo} alt="mirador4"></img>
         </div>
       </Link>
       <Link to="/miradores/5">
-        <div
-          className="home__mirador"
-          style={{ backgroundImage: `url(${mirador5photo})` }}
-        >
-          <p className="home__title">Desde El Castillo</p>{' '}
+        <div className="home__mirador">
+          <img src={mirador5photo} alt="mirador5"></img>
         </div>
       </Link>
       <Link to="/miradores/6">
-        <div
-          className="home__mirador"
-          style={{ backgroundImage: `url(${mirador6photo})` }}
-        >
-          <p className="home__title">Desde San Nicolás</p>{' '}
+        <div className="home__mirador">
+          <img src={mirador6photo} alt="mirador6"></img>
         </div>
       </Link>
       <Link to="/miradores/7">
-        <div
-          className="home__mirador"
-          style={{ backgroundImage: `url(${mirador7photo})` }}
-        >
-          <p className="home__title">Desde Puente Besson</p>{' '}
+        <div className="home__mirador">
+          {' '}
+          <img src={mirador7photo} alt="mirador7"></img>
         </div>
       </Link>
       <Link to="/miradores/8">
-        <div
-          className="home__mirador"
-          style={{ backgroundImage: `url(${mirador8photo})` }}
-        >
-          <p className="home__title">Desde Puente Santa María</p>{' '}
+        <div className="home__mirador">
+          <img src={mirador8photo} alt="mirador8"></img>
         </div>
       </Link>
     </div>
